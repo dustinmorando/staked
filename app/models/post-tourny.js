a4120469d1ff6fe521a79b69776eefc1a463d3e1
Dustin Morando <dustinmorando@me.com>
2015-06-21 16:39:03 -0400

    conflict resolved'

--- app/models/post-tourny.js
+++ app/models/post-tourny.js
@@ -6,30 +6,4 @@ export default DS.Model.extend({
   BuyIn: DS.attr('number'),
   EventName: DS.attr('string'),
   TournamentDate: DS.attr('date'),
-  
 });
-
-
-
-
-// // app/models/blog-post.js
-// var attr = DS.attr,
-//     hasMany = DS.hasMany;
-//
-// export default DS.Model.extend({
-//   title: attr(),
-//   createdAt: attr('date'),
-//
-//   comments: hasMany('comment')
-// });
-//
-// // app/models/comment.js
-// var attr = DS.attr,
-//     belongsTo = DS.belongsTo;
-//
-// export default DS.Model.extend({
-//   body: attr(),
-//   username: attr(),
-//
-//   post: belongsTo('blogPost')
-// });
